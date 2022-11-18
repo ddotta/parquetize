032500f2307dc9a5c897c3338f0e5c3fb2bfa50e
ddotta <damien.dotta@live.fr>
2022-11-18 12:52:08 +0000

    Add third test to csv_to_parquet

--- tests/testthat/test-csv_to_parquet.R
+++ tests/testthat/test-csv_to_parquet.R
@@ -1,5 +1,3 @@
-myfolder <- tempdir()
-
 test_that("Checks arguments are filled in", {
   expect_error(
     csv_to_parquet(
@@ -12,5 +10,15 @@ test_that("Checks arguments are filled in", {
       path_to_parquet = "Data"
     ),
     "Be careful, you have to fill in either the path_to_csv or url_to_csv argument"
+  )
+})
+
+test_that("Checks message is displayed", {
+  expect_message(
+    csv_to_parquet(
+      url_to_csv = "https://stats.govt.nz/assets/Uploads/Research-and-development-survey/Research-and-development-survey-2021/Download-data/research-and-development-survey-2021-csv.csv",
+      path_to_parquet = tempdir()
+    ),
+    paste0("The csv file is available in parquet format under ",tempdir())
   )
 })
