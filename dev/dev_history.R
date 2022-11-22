3db0777e2374f3d9cf214b83e85e1e56ee5c8aa1
Damien Dotta <damien.dotta@live.fr>
2022-11-22 13:16:06 +0100

    Corrections after check()

--- dev/dev_history.R
+++ dev/dev_history.R
@@ -21,7 +21,7 @@ usethis::use_package("readr")
 # usethis::use_pipe()
 
 # Pour réaliser le contrôle de conformité du package
-unlink("Data")
+unlink("Data",recursive = TRUE)
 devtools::check()
 
 # 3.c. Astuce qui peut aider durant le développement
