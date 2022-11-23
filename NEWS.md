3391d97d5158fdb41952283d61202c6113b85469
Damien Dotta <damien.dotta@live.fr>
2022-11-23 09:20:20 +0100

    close #8 Add check if path_to_parquet exists

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# parquetize 0.2.0
+
+* Check if `path_to_parquet` exists in functions `csv_to_parquet()` or `table_to_parquet()`
+
+
 # parquetize 0.1.0
 
 * Added `table_to_parquet()` function to convert SAS, SPSS and Stata files to parquet format.
--- R/table_to_parquet.R
+++ R/table_to_parquet.R
@@ -113,6 +113,11 @@ table_to_parquet <- function(
     stop("Be careful, the argument path_to_parquet must be filled in")
   }
 
+  # Check if path_to_parquet exists
+  if (dir.exists(path_to_parquet)==FALSE) {
+    dir.create(path_to_parquet, recursive = TRUE)
+  }
+
   update_progressbar(pbar = progressbar,
                      name_progressbar = conversion_progress,
                      value = 1)
