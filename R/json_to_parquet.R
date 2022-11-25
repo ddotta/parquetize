cb9382d89658ca7fbdbe65b6cebc0ad3f7690e7a
Damien Dotta <damien.dotta@live.fr>
2022-11-25 09:57:03 +0100

    Add json_to_parquet() function

---
+++ R/json_to_parquet.R
@@ -0,0 +1,115 @@
+#' Convert a json file to parquet format
+#'
+#' This function allows to convert a json file to parquet format. \cr
+#'
+#' Several conversion possibilities are offered :
+#'
+#'\itemize{
+#'
+#' \item{From a locally stored file. Argument `path_to_csv` must then be used;}
+#' \item{From a URL. Argument `url_to_csv` must then be used.}
+#'
+#' }
+#'
+#' @param path_to_json string that indicates the path to the csv file
+#' @param path_to_parquet string that indicates the path to the directory where the parquet file will be stored
+#' @param format string that indicates if the format is "json" (by default) or "ndjson"
+#' @param progressbar string () ("yes" or "no" - by default) that indicates whether you want a progress bar to display
+#' @param ... additional format-specific arguments, see \href{https://arrow.apache.org/docs/r/reference/write_parquet.html}{arrow::write_parquet()}
+#'  and \href{https://arrow.apache.org/docs/r/reference/write_dataset.html}{arrow::write_dataset()} for more informations.
+#' @return A parquet file, invisibly
+#'
+#' @importFrom jsonlite read_json
+#' @importFrom arrow write_parquet read_json_arrow
+#' @export
+#'
+#' @examples
+#'
+#' # Conversion from a local json file :
+#'
+#' json_to_parquet(
+#'   path_to_json = system.file("extdata","iris.json",package = "parquetize"),
+#'   path_to_parquet = tempdir(),
+#'   progressbar = "no"
+#' )
+#'
+#' # Conversion from a local ndjson file :
+#'
+#' json_to_parquet(
+#'   path_to_json = system.file("extdata","iris.ndjson",package = "parquetize"),
+#'   path_to_parquet = tempdir(),
+#'   format = "ndjson",
+#'   progressbar = "no"
+#' )
+
+json_to_parquet <- function(
+    path_to_json,
+    path_to_parquet,
+    format = "json",
+    progressbar = "yes",
+    ...
+) {
+
+
+  if (progressbar %in% c("yes")) {
+    # Initialize the progress bar
+    conversion_progress <- txtProgressBar(style = 3)
+  }
+
+  # Check if path_to_json is missing
+  if (missing(path_to_json)) {
+    stop("Be careful, the argument path_to_json must be filled in")
+  }
+
+  # Check if path_to_parquet is missing
+  if (missing(path_to_parquet)) {
+    stop("Be careful, the argument path_to_parquet must be filled in")
+  }
+
+  # Check if path_to_parquet exists
+  if (dir.exists(path_to_parquet)==FALSE) {
+    dir.create(path_to_parquet, recursive = TRUE)
+  }
+
+  # Check if format is equal to "json" or "ndjson"
+  if (!(format %in% c("json","ndjson"))) {
+    stop("Be careful, the argument format must be equal to 'json' or 'ndjson'")
+  }
+
+  update_progressbar(pbar = progressbar,
+                     name_progressbar = conversion_progress,
+                     value = 1)
+
+
+  if (format %in% c("json")) {
+    json_output <- jsonlite::read_json(path = path_to_json,
+                                       simplifyVector = TRUE,
+                                       ...)
+  } else if (format %in% c("ndjson")) {
+    json_output <- read_json_arrow(file = path_to_json,
+                                   as_data_frame = TRUE,
+                                   ...)
+  }
+
+  update_progressbar(pbar = progressbar,
+                     name_progressbar = conversion_progress,
+                     value = 6)
+
+  parquetname <- paste0(gsub("\\..*","",sub(".*/","", path_to_json)),".parquet")
+
+
+  parquetfile <- write_parquet(json_output,
+                               sink = file.path(path_to_parquet,
+                                                parquetname),
+                               ...
+  )
+
+  update_progressbar(pbar = progressbar,
+                     name_progressbar = conversion_progress,
+                     value = 10)
+
+  message(paste0("\nThe json file is available in parquet format under ",path_to_parquet))
+
+  return(invisible(parquetfile))
+
+}
