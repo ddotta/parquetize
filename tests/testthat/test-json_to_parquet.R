d50e65fb1f4d5665f69255a27ae4bf0a5dae5887
Damien Dotta <damien.dotta@live.fr>
2022-11-25 10:50:51 +0100

    Add tests by adding partition and partitioning argument to csv_to_parquet() and json_to_parquet()

--- tests/testthat/test-json_to_parquet.R
+++ tests/testthat/test-json_to_parquet.R
@@ -50,3 +50,17 @@ test_that("Checks message is displayed with ndjson file", {
     "The ndjson file is available in parquet format under Data"
   )
 })
+
+test_that("Checks message is displayed with by adding partition and partitioning argument", {
+
+  expect_message(
+    json_to_parquet(
+      path_to_json = system.file("extdata","iris.json",package = "parquetize"),
+      path_to_parquet = "Data",
+      progressbar = "no",
+      partition = "yes",
+      partitioning =  c("Species")
+    ),
+    "The json file is available in parquet format under Data"
+  )
+})
