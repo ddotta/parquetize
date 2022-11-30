5be43242c699bc63318d05f9704cd476411474df
Damien Dotta <damien.dotta@live.fr>
2022-11-30 17:06:52 +0100

    Correction to dbDisconnect()

--- R/sqlite_to_parquet.R
+++ R/sqlite_to_parquet.R
@@ -100,7 +100,7 @@ sqlite_to_parquet <- function(
 
   sqlite_output <- DBI::dbReadTable(con_sqlite, table_in_sqlite)
 
-  dbDisconnect(sqlite_output)
+  dbDisconnect(con_sqlite)
 
   update_progressbar(pbar = progressbar,
                      name_progressbar = conversion_progress,
