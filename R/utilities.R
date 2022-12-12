6fcb0b67385f15963292aa3a427cd7b6f0c7a3af
Damien Dotta <damien.dotta@live.fr>
2022-12-12 21:37:05 +0100

    rename bychunk_progress -> rbind_parquet()

--- R/utilities.R
+++ R/utilities.R
@@ -35,53 +35,17 @@ bychunk <- function(file_format, path_to_table, path_to_parquet, chunk_size, ski
   not_completed <- nrow(tbl) != 0
 
   if (isTRUE(not_completed)) {
+
+    parquetname <- paste0(gsub("\\..*","",sub(".*/","", path_to_table)))
+    parquetizename <- paste0(parquetname,sprintf("%d",skip+1),"-",sprintf("%d",skip+chunk_size),".parquet")
+
     write_parquet(tbl,
                   sink = file.path(path_to_parquet,
-                                   paste0("parquetize",sprintf("%d",skip),".parquet"))
+                                   parquetizename)
     )
+
+    cli_alert_success("\nThe {file_format} file is available in parquet format under {path_to_parquet}/{parquetizename}")
   }
 
   return(not_completed)
-}
-
-# Function to read parquet files in a folder, combine them using rbind,
-# write the result to a new parquet file, and delete the initial files
-#' Utility function that updates a progress bar
-#'
-#' This function read parquet files in a folder, combine them using rbind and
-#' write the result to a new parquet file. It also delete the initial files.
-#'
-#' @param folder a folder
-#' @param output_name name of the output parquet file
-#'
-#' @importFrom arrow read_parquet
-#'
-#' @noRd
-bychunk_process <- function(folder,
-                            output_name) {
-  # Get the list of files in the folder
-  files <- list.files(folder, pattern = "^parquetize.*\\.parquet$")
-
-  # Initialize an empty list to store the data frames
-  data_frames <- list()
-
-  # Loop through the files
-  for (file in files) {
-    # Read the parquet file into a data frame
-    df <- read_parquet(file.path(folder,file))
-
-    # Add the data frame to the list
-    data_frames[[file]] <- df
-  }
-
-  # Use rbind to combine the data frames into a single data frame
-  combined_df <- do.call(rbind, data_frames)
-
-  # Write the combined data frame to a new parquet file
-  write_parquet(combined_df, file.path(folder, output_name))
-
-  # Delete the initial parquet files
-  unlink(file.path(folder,files))
-
-  return(invisible(combined_df))
 }
