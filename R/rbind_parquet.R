f491a7dc42ac4671c3cbcad2568df76da8e2b16e
Damien Dotta <damien.dotta@live.fr>
2022-12-12 21:46:55 +0100

    Modify example in rbind_to_parquet_example()

--- R/rbind_parquet.R
+++ R/rbind_parquet.R
@@ -23,19 +23,22 @@
 #'   dir.create("output")
 #' }
 #'
-#' file.create(fileext = "output/parquetize0.parquet")
+#' file.create(fileext = "output/test_data1-4.parquet")
 #' write_parquet(data.frame(
 #'   x = c("a","b","c"),
 #'   y = c(1L,2L,3L)
 #' ),
+#' "output/test_data1-4.parquet")
 #'
-#' file.create(fileext = "output/parquetize500000.parquet")
+#' file.create(fileext = "output/test_data4-6.parquet")
 #' write_parquet(data.frame(
 #'   x = c("d","e","f"),
 #'   y = c(4L,5L,6L)
-#' ), "output/parquetize500000.parquet")
-
-#' test_data <- rbind_parquet(folder = "output", output_name = "test_data.parquet")
+#' ), "output/test_data4-6.parquet")
+#'
+#' test_data <- rbind_parquet(folder = "output",
+#'                            output_name = "test_data",
+#'                            delete_initial_files = FALSE)
 
 rbind_parquet <- function(folder,
                           output_name,
