6ebdf68aa4e3214b9a6de064437403c5af255323
Nicolas Chuche <nicolas@barna.be>
2023-03-26 08:55:48 +0200

    refactor: download_extract to handle all file's types
    Now download_extract will try to download, unzip, do nothing and
    return the file path for next function so it can always be used.

--- R/download_extract.R
+++ R/download_extract.R
@@ -1,42 +1,69 @@
-#' download file and uncompress if needed
+#' @name download_extract
 #'
-#' This method will download the file or use the local file
+#' @title download and uncompress file if needed
 #'
-#' If the file is a zip it :
+#' @description This function will download the file if the file is remote and
+#'   unzip it if it is zipped.  It will just return the input path argument if
+#'   it's neither. \cr
 #'
-#' * returns the file if zip is only one file
-#' * returns filename_in_zip if there's more than one file
+#' If the zip contains multiple files, you can use `filename_in_zip` to set the file you want to unzip and use.
 #'
-#' If the file is not a zip it returns immediately the path argument.
+#' You can pipe output on all `*_to_parquet` functions.
 #'
-#' @param path the input file
-#' @param filename_in_zip name of the csv file in the zip. Required if several csv are included in the zip.
 #'
-#' @return the name of a file
+#' @param path the input  file's path or url.
+#' @param filename_in_zip name of the csv file in the zip. Required if
+#'   several csv are included in the zip.
+#'
+#' @return the path to the usable (uncompressed) file, invisibly.
+#'
+#' @importFrom tools file_ext
 #' @export
 #'
 #' @examples
 #'
-#' download_extract(system.file("extdata","mtcars.csv.zip", package = "readr"))
+#' # 1. unzip a local zip file
+#' # 2. parquetize it
 #'
-#' download_extract(
+#' file_path <- download_extract(system.file("extdata","mtcars.csv.zip", package = "readr"))
+#' csv_to_parquet(
+#'   file_path,
+#'   path_to_parquet = tempdir()
+#' )
+#'
+#' # 1. download a remote file
+#' # 2. extract the file census2021-ts007-ctry.csv
+#' # 3. parquetize it
+#'
+#' file_path <- download_extract(
 #'   "https://www.nomisweb.co.uk/output/census/2021/census2021-ts007.zip",
 #'   filename_in_zip = "census2021-ts007-ctry.csv"
 #' )
+#' csv_to_parquet(
+#'   file_path,
+#'   path_to_parquet = tempdir()
+#' )
 #'
-#' download_extract("/my/local/file.csv")
+#' # the file is local and not zipped so :
+#' # 1. parquetize it
+#'
+#' file_path <- download_extract(parquetize_example("region_2022.csv"))
+#' csv_to_parquet(
+#'   file_path,
+#'   path_to_parquet = tempdir()
+#' )
 #'
 download_extract <- function(path, filename_in_zip) {
-  if (!is_zip(path)) return(path)
-
   if (is_remote(path)) {
-    zip_file <- curl_download(path,tempfile())
+    tmp_file <- curl_download(path,tempfile(fileext = file_ext(path)))
   } else {
-    zip_file <- path
+    tmp_file <- path
   }
 
-  csv_files <- unzip(zipfile=zip_file,exdir=tempfile())
-  names(csv_files) <- sub('.*/', '', csv_files)
+  if (!is_zip(path)) return(invisible(tmp_file))
+
+  csv_files <- unzip(zipfile=tmp_file,exdir=tempfile())
+  names(csv_files) <- basename(csv_files)
 
   if (length(csv_files) > 1 & missing(filename_in_zip)) {
     cli_alert_danger("Be careful, zip files contains more than one file, you must set filename_in_zip argument")
@@ -46,6 +73,6 @@ download_extract <- function(path, filename_in_zip) {
   } else {
     path <- csv_files[[1]]
   }
-  path
+  invisible(path)
 }
 
