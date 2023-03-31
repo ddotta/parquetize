bc1e456fc00e331b1082763eb7385353053feb24
ddotta <damien.dotta@live.fr>
2023-03-31 15:12:55 +0200

    evol: add minor changes to check_parquet function

--- R/parquetize_example.R
+++ R/parquetize_example.R
@@ -13,11 +13,31 @@
 #' @examples
 #' parquetize_example()
 #' parquetize_example("region_2022.csv")
+#' parquetize_example("iris_dataset")
 
 parquetize_example <- function(file = NULL) {
+  extension <- tools::file_ext(file)
+
+  # To show all example files contained in parquetize
   if (is.null(file)) {
     dir(system.file("extdata", package = "parquetize"))
-  } else {
-    system.file("extdata", file, package = "parquetize", mustWork = TRUE)
+  # To get the path to a file with an extension
+  } else if (extension != "") {
+    path_to_file <- system.file("extdata", file, package = "parquetize")
+    if (path_to_file == "") {
+      cli_alert_danger("Be careful, {file} doesn't exist in parquetize")
+      stop("")
+    } else {
+      system.file("extdata", file, package = "parquetize", mustWork = TRUE)
+    }
+  # To get the path to a dataset without an extension
+  } else if (extension == "") {
+    path_to_file <- system.file(file.path("extdata",file), package = "parquetize")
+    if (path_to_file == "") {
+      cli_alert_danger("Be careful, {file} doesn't exist in parquetize")
+      stop("")
+    } else {
+      system.file(file.path("extdata",file), package = "parquetize", mustWork = TRUE)
+    }
   }
 }
