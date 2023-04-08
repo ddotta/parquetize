930a030079dc4fb0d08e4abafdf329aabfc22e82
Nicolas Chuche <nicolas@barna.be>
2023-04-08 10:12:10 +0200

    refactor: add tests and simplify parquetize_example

--- R/parquetize_example.R
+++ R/parquetize_example.R
@@ -5,7 +5,7 @@
 #' @description parquetize comes bundled with a number of sample files in its `inst/extdata`
 #' directory. This function make them easy to access
 #'
-#' @param file Name of file. If `NULL`, the example files will be listed.
+#' @param file Name of file or directory. If `NULL`, the example files will be listed.
 #
 #' @return A character string
 #'
@@ -16,28 +16,14 @@
 #' parquetize_example("iris_dataset")
 
 parquetize_example <- function(file = NULL) {
-  extension <- tools::file_ext(file)
-
   # To show all example files contained in parquetize
   if (is.null(file)) {
-    dir(system.file("extdata", package = "parquetize"))
-  # To get the path to a file with an extension
-  } else if (extension != "") {
-    path_to_file <- system.file("extdata", file, package = "parquetize")
-    if (path_to_file == "") {
-      cli_alert_danger("Be careful, {file} doesn't exist in parquetize")
-      stop("")
-    } else {
-      system.file("extdata", file, package = "parquetize", mustWork = TRUE)
-    }
-  # To get the path to a dataset without an extension
-  } else if (extension == "") {
-    path_to_file <- system.file(file.path("extdata",file), package = "parquetize")
-    if (path_to_file == "") {
-      cli_alert_danger("Be careful, {file} doesn't exist in parquetize")
-      stop("")
-    } else {
-      system.file(file.path("extdata",file), package = "parquetize", mustWork = TRUE)
-    }
+    return(dir(system.file("extdata", package = "parquetize")))
   }
+
+  #To get the path to a file or a   directory
+  tryCatch(
+    system.file("extdata", file, package = "parquetize", mustWork = TRUE),
+    error = function(cond) cli_abort("Be careful, {file} doesn't exist in parquetize", class = "no_such_file")
+  )
 }
