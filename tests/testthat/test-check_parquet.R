15803532d5811c7319bb3466839d4461b945da9e
Damien Dotta <damien.dotta@live.fr>
2024-02-29 15:36:23 +0000

    fix: remove useless snapshots tests for table_to_parquet()

--- tests/testthat/test-check_parquet.R
+++ tests/testthat/test-check_parquet.R
@@ -11,15 +11,3 @@ test_that("check_parquet fails on missing file", {
     class = "no_such_file"
   )
 })
-
-test_that("check_parquet works on good dataset/file", {
-  expect_snapshot(
-    check_parquet(parquetize_example("iris_dataset")),
-    transform = function(str) sub("(checking: ).*", "\\1", str)
-  )
-
-  expect_snapshot(
-    check_parquet(parquetize_example("iris.parquet")),
-    transform = function(str) sub("(checking: ).*", "", str)
-  )
-})
